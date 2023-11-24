025c4ac043b51124c2f1970b1e06c8fb76a4313d
afsil6236@misena.edu.co <afsil6236@misena.edu.co>
2023-11-24 11:31:00 -1200

    token

--- middleware/auth.js
+++ middleware/auth.js
@@ -9,12 +9,12 @@ exports.protect = async(req, res, next)=>{
 
     let token 
     //1. vereficar si existe el header 'authorization'
-    if (req.headers.authozation && 
-        req.headers.authozation.
+    if (req.headers.authorization && 
+        req.headers.authorization.
         startsWith('Bearer')) {
             token = req.
                     headers.
-                    authozation.
+                    authorization.
                     split(' ')[1]
                                 }
     if (!token) {
@@ -24,11 +24,15 @@ exports.protect = async(req, res, next)=>{
                     success: false,
                     msg: "Invalid token"
                     })
-                }else{
+    }else{
                     const decoded=jwt.verify(token, 
                         process.env.JWT_SECRET_KEY)
-                        console.log(decoded)
-                }
+                        //console.log(decoded)
+                        //añadir al require en user
+                        req.user = await usersModel.findById(decoded.id)
+                        //redirigir a la ruta de bootcamps
+                        next()
+        }
 }
 
 //middleware para proteger de suurios que no tengan el rol especifico 
